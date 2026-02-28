cb4047733dfea16ab4451111f11de06303e275e2
CompleteNoName <coolrusguy@gmail.com>
2026-02-28 16:34:39 +0300

    Добавлен модуль для работы с хэшированными паролями

---
+++ apps/medical-appointment-backend/src/modules/password/index.ts
@@ -0,0 +1,19 @@
+import * as crypto from "crypto";
+
+class PasswordService {
+    static hash(pass: string): string {
+        const salt = crypto.randomBytes(16).toString("hex");
+        const hashBuf = crypto.scryptSync(pass, salt, 64);
+        return `${hashBuf.toString("hex")}.${salt}`;
+    };
+
+    static compare(hashedPass1: string, pass2: string): boolean {
+        const [pass1Hash, passSalt] = hashedPass1.split(".");
+        if (!(pass1Hash || passSalt)) throw Error("Malformed password passed to Password.compare - missing hash or salt");
+        const pass2HashBuf = crypto.scryptSync(pass2, passSalt as string, 64);
+        const pass1HashBuf = Buffer.from(pass1Hash as string, "hex");
+        return crypto.timingSafeEqual(pass1HashBuf, pass2HashBuf);
+    };
+};
+
+module.exports = PasswordService;
