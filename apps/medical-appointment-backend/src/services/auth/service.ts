4a8f348a886c8f9e6d959458169cfa06284f67b5
CompleteNoName <coolrusguy@gmail.com>
2026-03-01 00:01:43 +0300

    Реализован класс-сервис для микросервиса авторизации

---
+++ apps/medical-appointment-backend/src/services/auth/service.ts
@@ -0,0 +1,80 @@
+import AuthRepository from "./repository.js";
+import { AuthHandler } from "@modules/auth_handler";
+import { PasswordHandler } from "@modules/password";
+import { RegisterUserResult } from "@custom_types/registeruserresult";
+
+export default class AuthService {
+    constructor(private repo: AuthRepository) {}
+
+    async register(name: string, pass: string, role: 'P' | 'D' | 'A'): Promise<{
+        result: RegisterUserResult,
+        userId: string | null
+    }> {
+        const userInfo = await this.repo.getUserByName(name);
+        if (userInfo) return { result: RegisterUserResult.Duplicate, userId: null };
+
+        const passHash = PasswordHandler.hash(pass);
+        const userId = await this.repo.addUser(name, passHash, role);
+        if (!userId) return { result: RegisterUserResult.Error, userId: null };
+
+        return { result: RegisterUserResult.Success, userId };
+    }
+
+    async login(name: string, pass: string, userAgent?: string, userIp?: string): Promise<{
+        refreshToken: string,
+        accessToken: string
+    } | null> {
+        const userAuthData = await this.repo.getUserByName(name);
+        if (!userAuthData) return null;
+        if (!PasswordHandler.compare(userAuthData.passHash, pass)) return null;
+
+        const refreshTokenData = AuthHandler.generateRefreshToken();
+        const refreshTokenHash = AuthHandler.hashToken(refreshTokenData.token);
+        const tokenInsertResult = await this.repo.addRefreshToken(
+            userAuthData.userId, 
+            refreshTokenHash, 
+            refreshTokenData.exp,
+            userAgent,
+            userIp
+        );
+        if (!tokenInsertResult) return null;
+
+        const accessToken = AuthHandler.generateAccessToken({
+            user_id: userAuthData.userId,
+            user_name: name,
+            user_role: userAuthData.role
+        });
+
+        return {
+            accessToken,
+            refreshToken: refreshTokenData.token
+        };
+    }
+
+    async logout(token: string): Promise<boolean> {
+        const tokenHash = AuthHandler.hashToken(token);
+        const tokenDeleteResult = await this.repo.deleteRefreshToken(tokenHash);
+        return tokenDeleteResult; 
+    }
+
+    async refreshToken(token: string): Promise<string | null> {
+        const tokenHash = AuthHandler.hashToken(token);
+        const tokenData = await this.repo.getRefreshTokenByHash(tokenHash);
+        
+        if (!tokenData) return null;
+        if (tokenData.revoked || new Date(tokenData.exp) < new Date()) {
+            await this.repo.deleteRefreshToken(tokenHash);
+            return null;
+        }
+
+        const userInfo = await this.repo.getUserById(tokenData.userId);
+        if (!userInfo) return null;
+        
+        const newToken = AuthHandler.generateAccessToken({
+            user_id: tokenData.userId,
+            user_name: userInfo.name,
+            user_role: userInfo.role
+        });
+        return newToken;
+    }
+}
