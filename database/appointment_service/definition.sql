78594d51da85847117ddc90728eb4a43199ae961
CompleteNoName <coolrusguy@gmail.com>
2026-03-05 18:19:36 +0300

    Добавлено поле статуса записи на прием в БД

--- database/appointment_service/definition.sql
+++ database/appointment_service/definition.sql
@@ -1,14 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS appointments (
-    id              UUID        PRIMARY KEY,
-    patient_id      UUID        NOT NULL,
-    doctor_id       UUID        NOT NULL,
-    start_time      TIME        NOT NULL,
+    id              UUID            PRIMARY KEY,
+    patient_id      UUID            NOT NULL,
+    doctor_id       UUID            NOT NULL,
+    start_time      TIME            NOT NULL,
+    progress        VARCHAR(16)     CHECK(progress IN ('Назначен', 'Завершен', 'Отменен')) DEFAULT 'Назначен',
     patient_notes   TEXT,
     doctor_notes    TEXT,
-    created_at      TIMESTAMP   DEFAULT NOW(),
-    updated_at      TIMESTAMP   DEFAULT NOW(),
+    created_at      TIMESTAMP       DEFAULT NOW(),
+    updated_at      TIMESTAMP       DEFAULT NOW(),
     UNIQUE (doctor_id, start_time)
 );
 CREATE INDEX IF NOT EXISTS idx_appointments_patient_id ON appointments(patient_id);
